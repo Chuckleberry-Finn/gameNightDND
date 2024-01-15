8c449c756421e35b9ce77adcb0db77be7d29cb69
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-14 21:48:35 -0500

    ready?

---
+++ Contents/mods/main/media/lua/client/gameNight - implementDND.lua
@@ -0,0 +1,17 @@
+-- GAME PIECES
+---First require this file so that the gamePieceAndBoardHandler module can be called on.
+local gamePieceAndBoardHandler = require "gameNight - gamePieceAndBoardHandler"
+
+---Register game pieces by type -- enables the system to display the items using custom textures found in:
+--- `Item_InPlayTextures` and `Item_OutOfPlayTextures`
+--- Note: In-Play defaults to Out of play textures. Out of play textures replaces the item's texture/icon.
+gamePieceAndBoardHandler.registerTypes({"Base.StellaOcta","Base.Dice4", "Base.Dice6", "Base.Dice8", "Base.Dice10", "Base.Dice12", "Base.Dice20"})
+
+
+---Because I hate copy pasted code - this iterates through the side values and registers their special actions.
+local sides = {"4","6","8","10","12","20"}
+for _,side in pairs(sides) do
+    gamePieceAndBoardHandler.registerSpecial("Base.Dice"..side, { actions = { rollDie=side }, shiftAction = "rollDie", })
+end
+
+gamePieceAndBoardHandler.registerSpecial("Base.StellaOcta", { actions = { rollDie=1 }, shiftAction = "rollDie", })
---
+++ Contents/mods/main/media/lua/server/gameNight - DistributionsDND.lua
@@ -0,0 +1,25 @@
+require "Items/SuburbsDistributions"
+
+local gameNightDistro = require "gameNight - Distributions"
+
+table.insert(gameNightDistro.proceduralDistGameNight.itemsToAdd,"DNDBox")
+
+gameNightDistro.gameNightBoxes["DNDBox"] = {
+    rolls = 1,
+    items = {
+        "Dice4", 9999,
+        "Dice6", 9999,
+        "Dice8", 9999,
+        "Dice10", 9999,
+        "Dice12", 9999,
+        "Dice20", 9999,
+
+        "Pencil", 9999, "Pencil", 9999, "Eraser", 9999, "Eraser", 9999,
+
+        "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999,
+        "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999,
+        "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999, "SheetPaper2", 9999,
+
+    },
+    junk = { rolls = 1, items = {} }, fillRand = 0,
+}
