d33cf643bc19ecea2629eea6980a9bb66a0a29c4
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-23 23:28:03 -0500

    Update gameNight - DistributionsDND.lua

--- Contents/mods/game-night - DND/media/lua/server/gameNight - DistributionsDND.lua
+++ Contents/mods/game-night - DND/media/lua/server/gameNight - DistributionsDND.lua
@@ -14,10 +14,10 @@ gameNightDistro.gameNightBoxes["DNDBox"] = {
         "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999,
         "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999, "CharacterSheet", 9999,
 
-        --"DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
-        --"DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
-        --"DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
-        --"DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
+        "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
+        "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
+        "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
+        "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999, "DrawingPaper", 9999,
     },
     junk = { rolls = 1, items = {} }, fillRand = 0,
 }
