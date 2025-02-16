58a0d1360f38e1a85bb473a7e8995cbb1e203ee7
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-16 10:22:44 -0500

    Moved textures to /common for B42, and make B41 access B42 assets.

--- Contents/mods/game-night - DND/42/media/lua/server/gameNight - DistributionsDND.lua
+++ Contents/mods/game-night - DND/42/media/lua/server/gameNight - DistributionsDND.lua
@@ -5,6 +5,6 @@ local gameNightDistro = require "gameNight - Distributions"
 gameNightDistro.proceduralDistGameNight.itemsToAdd["DNDBox"] = {}
 
 gameNightDistro.gameNightBoxes["DNDBox"] = {
-    Dice_4 = 1, Dice_6 = 1, Dice_8 = 1, Dice_10 = 1, Dice_12 = 1, Dice_20 = 1, --StellaOcta = 0.1,
+    Dice_4 = 1, Dice_6 = 1, Dice_8 = 1, Dice_10 = 1, Dice_12 = 1, Dice_20 = 1, StellaOcta = 0.1,
     Pencil = 3, Eraser = 3, CharacterSheet = 12, DrawingPaper = 24,
 }
